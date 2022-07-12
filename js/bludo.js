7a144618ae64d58b8df1b7ccffa22ca4d743e586
Dastan Ernisov <dastanernisov8079@gmail.com>
2022-07-12 09:31:57 +0600

    delivery

---
+++ js/bludo.js
@@ -0,0 +1,32 @@
+let popup = document.querySelector(".bludo__popup");
+
+// Get the modal
+var popupBg = document.querySelector(".bludo__popup__bg");
+
+// Get the button that opens the modal
+var openPopupButtons = document.querySelectorAll(".bludo__btn");
+document.querySelector
+// Get the <span> element that closes the modal
+var closePopup = document.getElementsByClassName("bludo__close-popup")[0];
+
+// When the user clicks on the button, open the modal
+const buttonOpen = [...openPopupButtons];
+
+buttonOpen.map((item) => {
+  item.addEventListener("click", () => {
+    popupBg.style.display = "block";
+    popup.style.display = "block"
+  });
+});
+
+// When the user clicks on <span> (x), close the modal
+closePopup.onclick = function () {
+  popupBg.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == popupBg) {
+    popupBg.style.display = "none";
+  }
+};
